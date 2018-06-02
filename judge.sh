23a0f506b2f4efb92a5fba25774bb73ce8f09fb4
CroXX <39448470+Pega-Stellar@users.noreply.github.com>
2018-06-02 11:32:39 +0700

    Add files via upload

--- judge.sh
+++ judge.sh
@@ -38,10 +38,10 @@ fi
 printf "\t ${bold}$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps${plain}\t "
 # printf "\t ${bold}${purple}αβγδεζηθικλμνξοπρςτυφχψω${plain}\n\t"
 
-touch AClog.splog
-touch WAlog.splog
-touch RElog.splog
-touch TLElog.splog
+touch ".AClog.splog"
+touch ".WAlog.splog"
+touch ".RElog.splog"
+touch ".TLElog.splog"
 # 
 for xtest in $testFile/*/;
 do
@@ -53,53 +53,52 @@ do
 	let numtest++
 	inp="$xtest/$orgname.inp"
 	out="$xtest/$orgname.out"
-	if [ -e tmp.out ] ; then
-		> tmp.out
+	if [ -e ".tmp.out" ] ; then
+		> ".tmp.out"
 	fi
-	if timeout --foreground $tlmit $exeFile < $inp > tmp.out ; then
+	if timeout --foreground $tlmit $exeFile < $inp > ".tmp.out" ; then
 		:
 	else 
 		if [ $? == 124 ] ; then
-			printf "${white}${bold} † ${plain}${normal}"
-			echo " $testname " >> TLElog.splog
+			printf "\e[38;5;130m${bold} † ${plain}${normal}"
+			echo " $testname " >> ".TLElog.splog"
 			let tlepoint++
 		else
-			printf "\e[38;5;209m${bold} ø ${plain}${normal}"
-			echo " $testname " >> RElog.splog
+			printf "\e[38;5;195m${bold} ø ${plain}${normal}"
+			echo " $testname " >> ".RElog.splog"
 			let repoint++
 		fi
 		continue
 	fi
 	
-	if diff -Z $out tmp.out &> /dev/null; then
+	if diff -Z $out ".tmp.out" &> /dev/null; then
 		printf "${green}${bold} ✔ ${plain}${normal}"
 		let point++
-		echo " $testname " >> AClog.splog
+		echo " $testname " >> ".AClog.splog"
 	else
 		printf "${red}${bold} ✖ ${plain}${normal}"
 		let wapoint++
-		echo " $testname " >> WAlog.splog
+		echo " $testname " >> ".WAlog.splog"
 	fi
 done
-if [ -e tmp.out ]; then 
-	rm tmp.out 
+if [ -e ".tmp.out" ]; then 
+	rm ".tmp.out"
 fi
-printf "\n \t ${green}${bold}$point AC ${normal}/ ${white}$numtest test ${normal}\n"
+printf "\n \t ${green}${bold}$point AC ${normal}/ \e[38;5;191m${bold}$numtest test ${normal}\n"
 if [ $point = $numtest ] ; then
 	echo -e "\t ${blue}\e[7m~~~~~~~~~~Accepted~~~~~~~~~~ \e[27m"
 fi
 printf "\t ${bold}$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps$ms$ps${plain}\n"
 echo "$point / $numtest which : " >> $log
 echo "$repoint test RE " >> $log
-cat RElog.splog >> $log
+cat ".RElog.splog" >> $log
 echo "$tlepoint test TLE " >> $log
-cat TLElog.splog >> $log
+cat ".TLElog.splog" >> $log
 echo "$wapoint test WA " >> $log
-cat WAlog.splog >> $log
+cat ".WAlog.splog" >> $log
 echo "$point test AC " >> $log
-cat AClog.splog >> $log
-rm *.splog
+cat ".AClog.splog" >> $log
 
-echo $point > "xfoo.splog"
-echo $numtest > "xbar.splog"
+echo $point > ".xfoo.splog"
+echo $numtest > ".xbar.splog"
 
